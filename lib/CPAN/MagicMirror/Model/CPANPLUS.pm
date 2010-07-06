73dff06e4f6daf05a270f0e22f859e7a6951d12e
Aaron J Trevena <aaron.trevena@gmail.com>
2010-07-06 20:45:22 +0100

    fix copyright

--- lib/CPAN/MagicMirror/Model/CPANPLUS.pm
+++ lib/CPAN/MagicMirror/Model/CPANPLUS.pm
@@ -15,4 +15,17 @@ my @objs    = $cb->search(  type    => TYPE,
 
 
 
+=head1 AUTHOR
+
+Aaron Trevena, E<lt>teejay@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2010 by Aaron J Trevena
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.10.0 or,
+at your option, any later version of Perl 5 you may have available.
+
+
 1;
