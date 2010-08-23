e8aea793352ccb198ecc0b96105b62d5790b73e2
Aaron J Trevena <aaron.trevena@gmail.com>
2010-08-23 13:39:00 +0100

    added upload method/action

--- lib/CPAN/MagicMirror/Controller/Modules.pm
+++ lib/CPAN/MagicMirror/Controller/Modules.pm
@@ -8,11 +8,18 @@ CPAN::MagicMirror::Controller::Modules - Catalyst controller for modules
 
 =head1 DESCRIPTION
 
-Catalyst controller for modules 
+Catalyst controller for modules
 
 =cut
 
+use File::Path qw(make_path);
+use Archive::Any;
+use Module::Info;
+use CPAN::DistnameInfo;
+
 use parent 'Catalyst::Controller';
+
+my $module_file_path = 'modules';
 
 =head2 index
 
@@ -43,7 +50,6 @@ sub list : Local {
     warn "module list : $module_list\n";
     $c->stash->{module_list} = [ $module_list->all ];
     $c->stash->{pager} = $module_list->pager;
-    
     # specify template
     $c->stash->{template} = 'modules/list.tt';
 
@@ -82,6 +88,56 @@ sub search : Local {
     return;
 }
 
+=head2 upload
+
+handles uploading new clothing
+
+maps to /clothing/upload
+
+=cut
+
+sub upload : Local {
+    my ($self, $c) = @_;
+    $c->stash->{template} = 'upload.html';
+    # check for form submission
+    if ( $c->request->parameters->{form_submit} eq 'yes' ) {
+
+	# check for uploaded data
+	if ( my $upload = $c->request->upload('module_file') ) {
+	    my $filename = $upload->filename;
+
+	    my $author_id = $c->request->parameters->{author_id};
+	    my $module = $c->model('DB::Module')->create({author_id => $author_id, module_name => 'xxx', module_version=>0});
+	    my $destdir = $module_file_path . '/' . $module->id;
+	    my $uploaded_filename = "$destdir/$filename";
+
+	    unless ( $upload->link_to($uploaded_filename) || $upload->copy_to($uploaded_filename) ) {
+		die( "Failed to copy ’$filename’ to ’$uploaded_filename’: $!" );
+	    }
+
+	    my $archive;
+
+	    eval {
+		$archive=Archive::Any->new($uploaded_filename);
+		$archive->extract($destdir);
+	    };
+
+	    if (my $error=$@) {
+		$c->stash->{message} = "error unpacking/extracting file $uploaded_filename : $@";
+		return;
+	    }
+	    my $d = CPAN::DistnameInfo->new($filename);
+	    (my $dist_file = $d->dist) =~ s|\-|\/|g;      # "CPAN-DistnameInfo"
+	    my $mod = Module::Info->new_from_file("${dist_file}.pm");
+	    my $name    = $mod->name;
+	    my $version = $mod->version;
+	    my $dir     = $mod->inc_dir;
+	    my $file    = $mod->file;
+	    my $is_core = $mod->is_core;
+
+	}
+    }
+}
 
 
 =head1 AUTHOR
